2672f5dbc3b1df1e48e2bc73d0c38400b21eb8dc
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-06-11 22:32:15 +0530

    Anki Vector Music 🎶

---
+++ AnkiVectorMusic/config.py
@@ -0,0 +1,46 @@
+# AnkiVectorMusic- Telegram bot project
+# Copyright (C) 2021  Roj Serbest
+# Copyright (C) 2021  Damantha Jasinghe
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Modified by Damantha
+
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "ankivectorUpdates")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/94f98b19b2cd42aa50f4c.jpg")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "AnkivectorMusicbot")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "AnkiSupport_Official")
+PROJECT_NAME = getenv("PROJECT_NAME", "Anki Product")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/Damantha126/AnkiVecorMusic")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
