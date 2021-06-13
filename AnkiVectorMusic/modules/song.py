4f066ef07ce2d4b210a6721724238fefd0c9ec0f
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-06-13 19:13:57 +0530

    Anki Vector Music 🎶

--- AnkiVectorMusic/modules/song.py
+++ AnkiVectorMusic/modules/song.py
@@ -76,7 +76,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by @TheAnkiVectorbot **"
+        rep = "**🎶 Uploaded by @TheAnkiVectorbot **"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -138,8 +138,8 @@ async def progress(current, total, message, start, type_of_ps, file_name=None):
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
         progress_str = "{0}{1} {2}%\n".format(
-            "".join(["🔴" for i in range(math.floor(percentage / 10))]),
-            "".join(["🔘" for i in range(10 - math.floor(percentage / 10))]),
+            "".join(["❇️" for i in range(math.floor(percentage / 10))]),
+            "".join(["🅾️" for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2),
         )
         tmp = progress_str + "{0} of {1}\nETA: {2}".format(
