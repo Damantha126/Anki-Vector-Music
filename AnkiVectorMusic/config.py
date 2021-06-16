bb4ef7b8a17555a6b6e8823a0b1cd659a34b120d
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-06-16 10:17:29 +0530

    Anki Vector Music 🎶

--- AnkiVectorMusic/config.py
+++ AnkiVectorMusic/config.py
@@ -1,6 +1,6 @@
-# AnkiVectorMusic- Telegram bot project
+# AnkiVectorMusic Telegram bot project
 # Copyright (C) 2021  Roj Serbest
-# Copyright (C) 2021  Damantha Jasinghe
+# Copyright (C) 2021  Damantha_Jasinge
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Damantha
+# Modified by Damantha_Jasinge
 
 import os
 from os import getenv
@@ -28,15 +28,15 @@ SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "ankivectorUpdates")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/94f98b19b2cd42aa50f4c.jpg")
+BG_IMAGE = getenv("BG_IMAGE", "https://transfer.sh/1TZmz3J/6_15_2021%209_37_44%20PM.png")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "AnkivectorMusicbot")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "AnkiSupport_Official")
-PROJECT_NAME = getenv("PROJECT_NAME", "Anki Product")
-SOURCE_CODE = getenv("SOURCE_CODE", "github.com/Damantha126/AnkiVecorMusic")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DaisySupport_Official")
+PROJECT_NAME = getenv("PROJECT_NAME", "DaisyXMusic v4")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/Damantha126/Anki-Vector-Music")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
