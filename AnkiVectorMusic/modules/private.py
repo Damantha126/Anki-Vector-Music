ceb3c009fc9d1edd9395b508b7a2c850ed04010e
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-07-08 17:56:26 +0530

    Anki Vector Music 🎶

--- AnkiVectorMusic/modules/private.py
+++ AnkiVectorMusic/modules/private.py
@@ -21,7 +21,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from AnkiVectorMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
 logging.basicConfig(level=logging.INFO)
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['dkj']))
+@Client.on_message(filters.private & filters.incoming & filters.command(['start']))
 def _start(client, message):
     client.send_message(message.chat.id,
         text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
