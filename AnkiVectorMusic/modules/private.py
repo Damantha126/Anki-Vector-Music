ad604e813d70ba6d4a9e2f658247f84adec1a90a
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-07-26 07:58:17 +0530

    Anki Vector Music 🎶
    Updated

--- AnkiVectorMusic/modules/private.py
+++ AnkiVectorMusic/modules/private.py
@@ -21,7 +21,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from AnkiVectorMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
 logging.basicConfig(level=logging.INFO)
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['TrueCallerlogon']))
+@Client.on_message(filters.private & filters.incoming & filters.command(['start']))
 def _start(client, message):
     client.send_message(message.chat.id,
         text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
@@ -58,7 +58,7 @@ async def gstart(_, message: Message):
     )
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['Trueallerbyavbotzz']))
+@Client.on_message(filters.private & filters.incoming & filters.command(['help']))
 def _help(client, message):
     client.send_message(chat_id = message.chat.id,
         text = tr.HELP_MSG[1],
