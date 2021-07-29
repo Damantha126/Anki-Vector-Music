816443f5fa40f016f5156fd30a80a510097139fa
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-07-29 07:16:47 +0530

    Anki Vector Music 🎶

--- AnkiVectorMusic/services/callsmusic/callsmusic.py
+++ AnkiVectorMusic/services/callsmusic/callsmusic.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
