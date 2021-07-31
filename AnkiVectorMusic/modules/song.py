d637acf2207ec97a6d262e1b0272686f43e12ad5
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-07-31 08:05:36 +0530

    Anki Vector Music 🎵

--- AnkiVectorMusic/modules/song.py
+++ AnkiVectorMusic/modules/song.py
@@ -76,7 +76,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by @TheAnkiVectorbot **"
+        rep = "**🎵 Uploaded by @TheAnkiVectorBot**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -414,7 +414,7 @@ async def ytmusic(client, message: Message):
         progress_args=(
             pablo,
             c_time,
-            f"`Uploading {urlissed} Song From YouTube Music!`",
+            f"`Uploading {urlissed} Song From Anki Vector Music!`",
             file_stark,
         ),
     )
