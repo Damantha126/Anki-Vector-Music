827a4f5b7c27e83314be4df9a39a6fc4c341a3d1
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-08-03 06:47:28 +0530

    Anki Vector Music 🎵

---
+++ AnkiVectorMusic/helpers/modhelps.py
@@ -0,0 +1,44 @@
+# This File Is Made and Owned By @TheHamkerCat and @TeamDaisyX
+
+import socket
+import heroku3
+
+
+async def _netcat(host, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(content.encode())
+    s.shutdown(socket.SHUT_WR)
+    while True:
+        data = s.recv(4096).decode("utf-8").strip("\n\x00")
+        if not data:
+            break
+        return data
+    s.close()
+
+
+async def paste(content):
+    link = await _netcat("ezup.dev", 9999, content)
+    return link
+
+
+# Updator
+
+def fetch_heroku_git_url(api_key, app_name):
+    if not api_key:
+        return None
+    if not app_name:
+        return None
+    heroku = heroku3.from_key(api_key)
+    try:
+        heroku_applications = heroku.apps()
+    except:
+        return None
+    heroku_app = None
+    for app in heroku_applications:
+        if app.name == app_name:
+            heroku_app = app
+            break
+    if not heroku_app:
+        return None
+    return heroku_app.git_url.replace("https://", "https://api:" + api_key + "@")
